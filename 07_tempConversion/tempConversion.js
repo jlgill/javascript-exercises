45d4dc888573bd75ebf94285e077c83d49c04ce4
James Gill <mcdeth187@gmail.com>
2024-04-08 22:43:36 -0700

    more changes

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(fahrenheit) {
+  // Conversion formula: (°F - 32) * 5/9
+  return Number(((fahrenheit - 32) * 5 / 9).toFixed(1));
+}
 
-const convertToFahrenheit = function() {
-};
-
+// Function to convert Celsius to Fahrenheit
+function convertToFahrenheit(celsius) {
+  // Conversion formula: (°C * 9/5) + 32
+  return Number(((celsius * 9 / 5) + 32).toFixed(1));
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
