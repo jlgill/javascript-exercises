466e73c7052f8de47810c1698a9c703f4ee9d934
James Gill <mcdeth187@gmail.com>
2024-04-08 22:30:07 -0700

    more fixes

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Leap years are divisible by 4, but not by 100 unless also divisible by 400
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 };
 
 // Do not edit below this line
