1dc72e8d56b047d6e3980db7bcb3c69d05cf762b
James Gill <mcdeth187@gmail.com>
2024-04-08 22:27:04 -0700

    additional tests for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,23 @@
-const sumAll = function(startNum,stopNum) {
+function sumAll(startNum,stopNum){
+    //Check if the numbers are negative and return an error
+    if (typeof startNum !== 'number' || typeof stopNum !== 'number' || startNum < 0 || stopNum < 0){
+        return 'ERROR';
+    }
+
+    //Determin the smaller and larger numbers between startNum and stopNum
+    const minNum = Math.min(startNum, stopNum);
+    const maxNum = Math.max(startNum, stopNum);
+
+    //initiatlze sum
     let sum = 0;
-    for(let i = startNum ; i <= stopNum, i++){
+
+    //Loop through the range and add each number to the sum
+    for (let i = minNum; i <= maxNum; i++){
         sum += i;
     }
-};
+    return sum;
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
